8a472202f0546ec764453af2be5ef5efc7acbc39
petr_pes <ivanzhirnov170991@outlook.com>
2017-11-18 01:42:24 +0300

    Basics

---
+++ Assets/Scripts/Level/Room/PrefabId.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Levels.Rooms.RoomCreator
+{
+    [System.Serializable]
+    public struct PrefabId
+    {
+        [SerializeField] private ushort _prefabId;
+
+        public PrefabId(int prefabId)
+        {
+            if (Check(prefabId))
+            {
+                _prefabId = Convert.ToUInt16(prefabId);
+            }
+            else
+            {
+                _prefabId = 0;
+            }
+        }
+
+        public int Value
+        {
+            get
+            {
+                return _prefabId;
+            }
+            set
+            {
+                if (Check(value))
+                {
+                    _prefabId = Convert.ToUInt16(value);
+                }
+                else
+                {
+                    _prefabId = 0;
+                }
+            }
+        }
+
+        private static bool Check(int id)
+        {
+            if (id <= ushort.MinValue || id >= ushort.MaxValue)
+            {
+                Debug.LogError("Id should be in range with ushort type");
+                return false;
+            }
+            return true;
+        }
+    }
+}
+
